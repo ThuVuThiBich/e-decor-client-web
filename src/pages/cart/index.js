5bf3ca7f9b5e4f57f057c75d65d6a4657ce3a96a
ThuVuThiBich <vuthibichthu.iter@gmail.com>
2022-02-21 21:05:37 +0700

    fix UI cart page - empty cart

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,4 +1,4 @@
-import { Container } from "@material-ui/core";
+import { Box, Button, Container } from "@material-ui/core";
 import CartItems from "components/cart";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,6 +7,8 @@ import { cartSelector, userSelector } from "redux/selectors";
 import { useStyles } from "./styles";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { size } from "underscore";
+import Images from "constants/image";
 
 export default function Cart() {
   const classes = useStyles();
@@ -19,9 +21,28 @@ export default function Cart() {
   }, [currentUser, dispatch, isUpdated]);
   return (
     <Container className={classes.container}>
-      {products?.map((item, index) => (
-        <CartItems item={item} key={index} />
-      ))}
+      {size(products) ? (
+        products?.map((item, index) => <CartItems item={item} key={index} />)
+      ) : (
+        <Box
+          mt={8}
+          p={2}
+          px={6}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <img src={Images.EMPTY_CART} alt="" width={200} />
+          <Box m={2} style={{ color: "#bdbdbd" }}>
+            Your shopping cart is empty
+          </Box>
+          <Box mb={16}>
+            <Button variant="contained" color="primary">
+              Go Shopping Now
+            </Button>
+          </Box>
+        </Box>
+      )}
       <ToastContainer
         hideProgressBar
         autoClose={1000}
